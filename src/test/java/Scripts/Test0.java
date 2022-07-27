eef7e5718dab0a91707578f6a7eeaac4b0406518
Manish <Manish@192.168.1.33>
2022-07-27 16:17:42 +0530

    Jenkins Demo

---
+++ src/test/java/Scripts/Test0.java
@@ -0,0 +1,13 @@
+package Scripts;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Test0 {
+	
+	@Test
+	public void TestCase1() {
+		Reporter.log("Hello Java", true);
+	}
+
+}
---
+++ src/test/java/Scripts/Test3.java
@@ -0,0 +1,14 @@
+package Scripts;
+
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Test3 {
+	
+	@Test
+	public void TestCase1() {
+		Reporter.log("Hello Java,Selenium,Python", true);
+	}
+
+}
